40cb79d911a195c4a9311a240a8ef8b9db38af03
hrxgaurav <magicgalaxy0@gmail.com>
2023-05-15 15:05:17 +0530

    1.06

---
+++ src/Component/Form.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import useGeoLocation from "../Hooks/useGeoLocation";
+
+const Form = () => {
+  const [IP, setIP] = useState();
+
+  const location = useGeoLocation();
+
+  const scriptURL =
+    "https://script.google.com/macros/s/AKfycbwEptCCNw6hFnIPLkcerNNgFIC_mlQ0CtGkViOu7xci0nt7M5c_B8EhoSmtOMkBZ2HYeA/exec";
+  const form = document.forms["submit-to-google-sheet"];
+
+  const submit = (e) => {
+    e.preventDefault();
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+      .then((response) => console.log("Success!", response))
+      .catch((error) => console.error("Error!", error.message));
+  };
+
+  const currentDate = new Date(Date.now());
+  let day = currentDate.getDate();
+  let month = currentDate.getMonth() + 1;
+  let year = currentDate.getFullYear();
+  day = day < 10 ? "0" + day : day;
+  month = month < 10 ? "0" + month : month;
+  let formattedDate = day + "/" + month + "/" + year;
+
+  const getIp = async () => {
+    try {
+      const response = await fetch("https://api.ipify.org/?format=json");
+      const data = await response.json();
+      const ipAddress = data.ip;
+      setIP(ipAddress);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  getIp();
+
+
+
+  return (
+    <>
+      <form name="submit-to-google-sheet" onSubmit={submit}>
+        <input name="Date" type="text" placeholder="" value={formattedDate} />
+        <input
+          name="Time"
+          type="text"
+          placeholder=""
+          value={currentDate.toLocaleTimeString()}
+        />
+        <input
+          name="Location"
+          type="text"
+          value={
+            location.loaded
+              ? JSON.stringify(location.mapLink)
+              : "Location data not available yet." + location.error.message
+          }
+        />
+        <input name="IP" type="text" value={IP} />
+        <input name="RAM" type="text" />
+        <button type="submit">Send</button>
+      </form>
+      <h1>{}</h1>
+      <h1>{JSON.stringify(navigator.userAgentData)}</h1>
+    </>
+  );
+};
+
+export default Form;
--- src/Component/Geo.js
+++ src/Component/Geo.js
@@ -1,95 +1,16 @@
-import React, {  useState } from "react";
+import React from "react";
 import os from 'os-browserify'
 
 // console.log(os.platform(), os.hostname());
 
 const Geo = () => {
-  const [geo, setGeo] = useState();
-  const [time, setTime] = useState();
-  const [LocationDidMount, setLocationDidMount] = useState()
-
-  // const options = {
-  //   enableHighAccuracy: true,
-  //   timeout: 50000,
-  //   maximumAge: 0,
-  // };
-
-  // function success(pos) {
-  //   const crd = pos.coords;
-  //   setTime(new Date(Date.now()).toLocaleString());
-
-  //   // console.log("Your current position is:");
-  //   // console.log(`Latitude : ${crd.latitude}`);
-  //   // console.log(`Longitude: ${crd.longitude}`);
-  //   // console.log(`More or less ${crd.accuracy} meters.`);
-  //   setGeo(
-  //     `https://www.google.com/maps/search/${crd.latitude},+${crd.longitude}?shorturl=1`
-  //   );
-  // }
-
-  // function error(err) {
-  //   setGeo(err.message)
-  //   console.warn(`ERROR(${err.code}): ${err.message}`);
-  //   setTime(new Date(Date.now()).toLocaleString());
-  // }
-
-  // navigator.geolocation.watchPosition(success, error, options);
-
-  //   ----------------------------------------------------------------------------------------
-  //   const scriptURL = "https://script.google.com/macros/s/AKfycbwEptCCNw6hFnIPLkcerNNgFIC_mlQ0CtGkViOu7xci0nt7M5c_B8EhoSmtOMkBZ2HYeA/exec";
-  //   const form = document.forms["submit-to-google-sheet"];
-
-  //   form.addEventListener("submit", (e) => {
-  //     e.preventDefault();
-  //     fetch(scriptURL, { method: "POST", body: new FormData(form) })
-  //       .then((response) => console.log("Success!", response))
-  //       .catch((error) => console.error("Error!", error.message));
-  //   });
-
-
-  function getUserLocation() {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        function (position) {
-          setLocationDidMount(true);
-          console.log("got position");
-        },
-        function errorCallback(error) {
-          console.log(error);
-          navigator.permissions
-            .query({ name: "geolocation" })
-            .then(function (result) {
-              if (result.state === "granted") {
-                console.log(result.state);
-                //If granted then you can directly call your function here
-              } else if (result.state === "prompt") {
-                console.log(result.state);
-              } else if (result.state === "denied") {
-                console.log("location access denied by user");
-                //If denied then you have to show instructions to enable location
-              }
-              result.onchange = function () {
-                console.log(result.state);
-              };
-            });
-        },
-        {
-          timeout: 5000,
-        }
-      );
-    } else {
-      console.log("Not Available");
-    }
-  }
-
-  getUserLocation()
-
-  
 
   return <>
-    <h1>{time + " " +geo}</h1>
-    {time!==undefined ? console.log(time,geo): null}
+    
     <h2>Platform and hostName : {os.platform()+":"+ os.hostname()}</h2>
+    <div className="inline-block mr-auto pt-1">
+                                
+                          </div>
   </>;
 };
 
